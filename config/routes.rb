431271ddb1773687503420026bc080293feb3655
shimbiro <shimbirosharon@gmail.com>
2016-11-18 21:51:59 +0300

    irock

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+	resource :achievement, only: [ :new, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'welcome#index'
+  get 'achievement' => 'achievement#create'
+  get 'achievements' => 'achievement/new'
+
 end
